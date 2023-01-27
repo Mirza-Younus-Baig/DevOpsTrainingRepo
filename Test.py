99e086e6447533fdeb49b4405f7705200171a060
Mirza-EZTS <mirza@ezts.in>
2023-01-27 00:05:09 -0500

    Merge Conflicts Test

--- Test.py
+++ Test.py
@@ -14,10 +14,12 @@ print("Divide Output: ", res2)
 res3 = add(a,c)
 print("Add Output: ", res3)
 
-res4 = exponent(a,c)
-print("Exponent Output: ", res4)
 res5 = subtract(a,c)
 print("Subtract Output: ", res5)
+
+res4 = exponent(a,c)
+print("Exponent Output: ", res4)
+
 
 # # Create a list of random numbers of size res1
 # res2 = [random.randint(1, 10) for i in range(res1)]
